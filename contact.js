01422979656f7b1b3f6193a94a29b6b8c5a2c116
sukanya2712 <sukanyanaik511@gmail.com>
2023-04-20 21:59:35 +0530

    addeduc1

---
+++ contact.js
@@ -0,0 +1,12 @@
+class Contact {
+  constructor(firstName, lastName, address, city, state, zip, phone, email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.address = address;
+    this.city = city;
+    this.state = state;
+    this.zip = zip;
+    this.phone = phone;
+    this.email = email;
+  }
+}
