e2dab0f4bbdb95f42df55e317030f77f1b13298b
sukanya2712 <sukanyanaik511@gmail.com>
2023-04-21 10:53:30 +0530

    added

--- contact.js
+++ contact.js
@@ -1,3 +1,4 @@
+const readline = require('readline');
 class Contact {
 
     constructor(firstName, lastName, address, city, state, zip, phone, email) {
@@ -39,11 +40,144 @@ class Contact {
     }
   }
   
-  try {
-    const myContact = new Contact('Sukanya', 'Naik', 'srishti', 'virar', 'maharhtra', '12345', '982-234-5671', 'sukanya.naik@example.com');
-    console.log(myContact);
-  } catch (error) {
-    console.error(error);
+
+  
+  let addressBooks = [];
+
+ // function to prompt user for a yes/no question
+function promptYesNo(question, callback) {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+    rl.question(`${question} (y/n)`, (answer) => {
+      rl.close();
+      callback(answer.toLowerCase() === "y");
+    });
   }
   
-
+  // create new address book
+  function createAddressBook(callback) {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+    rl.question("Enter a name for the new address book:", (name) => {
+      rl.close();
+      let addressBook = {
+        name: name,
+        contacts: [] // initialize empty array for contacts in this address book
+      };
+      addressBooks.push(addressBook); // add new address book to array of address books
+      callback();
+    });
+  }
+  
+  // find address book by name
+  function findAddressBookByName(name) {
+    for (let i = 0; i < addressBooks.length; i++) {
+      if (addressBooks[i].name === name) {
+        return i; // return index of address book if found
+      }
+    }
+    return -1; // return -1 if address book not found
+  }
+  
+  function addContact(callback) {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+  
+    rl.question("Enter the name of the address book to add the contact to:", (name) => {
+      let index = findAddressBookByName(name);
+  
+      if (index === -1) {
+        // address book not found
+        promptYesNo("Address book not found. Do you want to create a new one?", () => {
+          createAddressBook(() => {
+            addContact(callback);
+          });
+        });
+        return; // exit function and wait for user input
+      }
+  
+      rl.question("Enter the first name of the new contact:", (firstName) => {
+        rl.question("Enter the last name of the new contact:", (lastName) => {
+          rl.question("Enter the address of the new contact:", (address) => {
+            rl.question("Enter the city of the new contact:", (city) => {
+              rl.question("Enter the state of the new contact:", (state) => {
+                rl.question("Enter the zip code of the new contact:", (zip) => {
+                  rl.question("Enter the phone number of the new contact (format: xxx-xxx-xxxx):", (phone) => {
+                    rl.question("Enter the email address of the new contact:", (email) => {
+                      let newContact = new Contact(firstName, lastName, address, city, state, zip, phone, email);
+                      addressBooks[index].contacts.push(newContact);
+                      rl.close();
+                      callback();
+                    });
+                  });
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  }
+  
+  function displayAddressBooks() {
+    console.log("Address Books:");
+    console.log("--------------");
+    for (let i = 0; i < addressBooks.length; i++) {
+      console.log(`Name: ${addressBooks[i].name}`);
+      console.log("Contacts:");
+      console.log("---------");
+      for (let j = 0; j < addressBooks[i].contacts.length; j++) {
+        let contact = addressBooks[i].contacts[j];
+        console.log(`Name: ${contact.firstName} ${contact.lastName}`);
+        console.log(`Address: ${contact.address}`);
+        console.log(`City: ${contact.city}`);
+        console.log(`State: ${contact.state}`);
+        console.log(`Zip: ${contact.zip}`);
+        console.log(`Phone: ${contact.phone}`);
+        console.log(`Email: ${contact.email}`);
+        console.log("--------");
+      }
+      console.log("--------------");
+    }
+  }
+  
+  // main function
+  function runAddressBookProgram() {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+    rl.question("Enter 1 to create a new address book : ,   2 to add a contact :, or   3 to exit : , 4 to display addressbooks and contacts : ", (action) => {
+      rl.close();
+      switch (action) {
+        case "1":
+          createAddressBook(() => {
+            runAddressBookProgram();
+          });
+          break;
+        case "2":
+          addContact(() => {
+            runAddressBookProgram();
+          });
+          break;
+        case "3":
+          return;
+        case "4":
+            displayAddressBooks();
+            runAddressBookProgram();
+            break;  
+        default:
+          console.log("Invalid input. Please try again.");
+          runAddressBookProgram();
+      }
+    });
+  }
+  
+  // run the program
+  runAddressBookProgram();
